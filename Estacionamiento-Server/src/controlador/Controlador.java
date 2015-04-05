27716e1a2a176cf45a72eb8a5c5afcb70449f347
Moreh89 <moredamian@gmail.com>
2015-04-05 12:12:37 -0300

    a

--- Estacionamiento-Server/src/controlador/Controlador.java
+++ Estacionamiento-Server/src/controlador/Controlador.java
@@ -1,101 +1,132 @@
 package controlador;
 
+import java.sql.Connection;
+
 import modelo.Cliente;
 import modelo.Tarifa;
 import modelo.Ticket;
+import modelo.Usuario;
+import persistencia.PoolConnection;
 
 public class Controlador {
 
 	private Tarifa tarifa;
 	private Cliente cliente;
 	private Ticket ticket;
+	private Usuario usuarioActual;
 
-	private static Controlador controlador;
-	
-	public static Controlador getInstancia()
-	{
-		if(controlador==null)
-		{
-			controlador=new Controlador();
-		}
-		return controlador;
-	}
-	
-	
+	private static Controlador instancia;
+
 	public static void main(String []args)
 	{
 
 	}
 
-	
-	
-	public double cobrar(int cantidadMinutos)
+	public static Controlador getInstancia()
 	{
-		double montoCobrar=0;        
-		
-		if(cliente.equals("porHora"))
+		if(instancia==null)
 		{
-			montoCobrar=cobroPorHora(cantidadMinutos, tarifa);
+			instancia=new Controlador();
 		}
-		else if(cliente.equals("porEstadia"))
-		{
-			montoCobrar=cobroPorEstadia(cantidadMinutos, tarifa);
-		}
-		return montoCobrar;
+		return instancia;
 	}
 
-		
-	
-	private static double cobroPorHora(double cantidadMinutos, Tarifa tarifa)
-	{
-		double montoCobrar=0;
-		if(cantidadMinutos<60)
-		{
-			montoCobrar=tarifa.getCostoMinimo();
-		}
-		else
-		{
-			//Calculo Multiplo comun maximo -> puede que haya function.
-			double countHs=(int)(cantidadMinutos/60);
-			montoCobrar=montoCobrar+tarifa.getCostoHora()*countHs;
-			cantidadMinutos=cantidadMinutos-countHs*60;
-			//con el Math.ceil redondeo al techo.
-			double countFracciones=(int)Math.ceil((cantidadMinutos)/tarifa.getTiempoFraccion());
-			montoCobrar=montoCobrar+tarifa.getCostoFraccion()*countFracciones;
-		}
-		return montoCobrar;
+	public boolean validarContrasenaActual(int dni , String psw){
+
+		Usuario u = buscarUsuario(dni) ;
+
+		if (u!=null){
+			if(psw.equals(u.getPassword())){
+				return true;
+			}else
+				return false;
+		}else
+			return false;
 	}
 
-	private static double cobroPorEstadia(double cantidadMinutos, Tarifa tarifa)
-	{
-		double montoCobrar=0;
+	public boolean validarLogin(String userName, String password){
 
-		if(cantidadMinutos<=tarifa.getTiempoMediaEstadia_min())
-		{
-			montoCobrar=tarifa.getCostoMediaEstadia();
-		}
-		else if(cantidadMinutos<=tarifa.getTiempoEstadia_min())
-		{
-			montoCobrar=tarifa.getCostoEstadia();
-		}
-		else
-		{
-			double cantidadHsEstadia=(int)(cantidadMinutos/tarifa.getTiempoEstadia_min());
-			montoCobrar=montoCobrar+tarifa.getCostoEstadia()*cantidadHsEstadia;
-			cantidadMinutos=cantidadMinutos-cantidadHsEstadia*tarifa.getTiempoEstadia_min();			
-			double cantidadHsMediaEstadia=(int)(cantidadMinutos/tarifa.getTiempoMediaEstadia_min());
-			if(cantidadHsMediaEstadia>=0)
-			{
-				montoCobrar=montoCobrar+tarifa.getCostoMediaEstadia()*cantidadHsMediaEstadia;
-				cantidadMinutos=cantidadMinutos-cantidadHsMediaEstadia*tarifa.getTiempoMediaEstadia_min();
-			}			
-			//con el Math.ceil redondeo al techo.
-			double countFracciones=(int)Math.ceil((cantidadMinutos)/tarifa.getTiempoFraccion());
-			montoCobrar=montoCobrar+tarifa.getCostoFraccion()*countFracciones;
-		}
+		String pswEnc = Encriptacion.Encriptar(password);
 
+		Usuario u = this.buscarUsuario(userName, pswEnc) ;
 
-		return montoCobrar;
+		if (u!=null){
+
+			if(pswEnc.equals(u.getPassword())){
+				usuarioActual = u.getView();
+				return true;
+			}
+			else
+				return false;
+		}
+		return false;
 	}
+
+
+
+
+	public Usuario buscarUsuario(String userName, String password){
+
+		Usuario u = null ;
+
+		if (!usuarios.isEmpty()){
+			for (Usuario usuarioTemp : this.usuarios) {
+				if (usuarioTemp.getUserName().equals(userName)){
+					u = usuarioTemp;
+					break;
+				}
+			}
+		}
+		if (u == null){
+
+			u = new Usuario().buscarUsuario(userName);
+			this.usuarios.add(u);
+
+		}
+
+		if (u!=null){
+			return u;
+		}else
+			return null;
+	}
+
+
+	public Usuario buscarUsuario(int dni){
+
+		Usuario u = null ;
+
+		if (!usuarios.isEmpty()){
+			for (Usuario usuarioTemp : this.usuarios) {
+				if (usuarioTemp.getDni() == dni)
+				{
+					u = usuarioTemp;
+					break;
+				}
+			}
+		}
+
+		if (u == null){
+			u = new Usuario().buscarUsuario(dni);
+			this.usuarios.add(u);
+		}
+
+		return u;
+	}
+
+	public Usuario getUsuarioActual() {
+		return usuarioActual;
+	}
+
+
+	public boolean probarConexion(){
+
+		Connection con = PoolConnection.getPoolConnection().getConnection();
+		if (con == null){
+			return false;
+		}else
+			return true;
+	}
+
+
 }
 
---
+++ Estacionamiento-Server/src/modelo/Usuario.java
@@ -0,0 +1,58 @@
+package modelo;
+
+public class Usuario 
+{
+	
+	String userName;
+	String password;
+	String permisos;
+	String nombre;
+	String apellido;
+	String tipoDocumento;
+	String numeroDocumento;
+	public String getPermisos() {
+		return permisos;
+	}
+	public void setPermisos(String permisos) {
+		this.permisos = permisos;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTipoDocumento() {
+		return tipoDocumento;
+	}
+	public void setTipoDocumento(String tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+	public String getNumeroDocumento() {
+		return numeroDocumento;
+	}
+	public void setNumeroDocumento(String numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
